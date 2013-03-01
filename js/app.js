bab9f290668f494c0d0aebf48156001f31dd8aba
Tristan O'Neil <tristanoneil@gmail.com>
2013-02-28 19:02:17 -0500

    Refine styles. Add call for proposal links.

--- js/app.js
+++ js/app.js
@@ -3,15 +3,7 @@ $(function() {
     $(this).addClass("fadeIn");
   });
 
-  $("#intro").delay(1400).queue(function() {
-    $(this).addClass("fadeIn");
-  });
-
-  $("#email").delay(2400).queue(function() {
-    $(this).addClass("fadeIn");
-  });
-
-  $("#footer").delay(3400).queue(function() {
+  $("#content").delay(1400).queue(function() {
     $(this).addClass("fadeIn");
   });
 });
